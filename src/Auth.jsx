9c3a824af7f167064c711f9025ded940bc0e5ab2
hashi.hiro.0531@gmail.co.jp <hashi.hiro.0531@gmail.co.jp>
2021-07-25 22:49:51 +0900

    listen-auth

---
+++ src/Auth.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ListenAuthState } from './redux/users/operations';
+import { getIsSignedIn } from './redux/users/selectors';
+
+const Auth = ({ children }) => {
+  const dispatch = useDispatch();
+  const selector = useSelector(state => state);
+  const isSignedIn = getIsSignedIn(selector);
+  useEffect(() => {
+    if (!isSignedIn) {
+      dispatch(ListenAuthState());
+    }
+  }, []);
+
+  if (!isSignedIn) {
+    return <>こんにちは</>;
+  } else {
+    return children;
+  }
+};
+
+export default Auth;
--- src/Router.jsx
+++ src/Router.jsx
@@ -2,14 +2,16 @@ import { Route, Switch } from 'react-router';
 import { Home, SignUp, SignIn } from './templates';
 import './assets/reset.css';
 import './assets/style.css';
+import Auth from './Auth';
 
 const Router = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-
       <Route exact path="/signup" component={SignUp} />
       <Route exact path="/signin" component={SignIn} />
+      <Auth>
+        <Route exact path="/" component={Home} />
+      </Auth>
     </Switch>
   );
 };
